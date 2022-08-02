a03567bd076f6405e0902091b09cc6f10c64e16d
Fatema04 <shadrinmoni15@gmail.com>
2022-08-01 18:53:19 -0700

    reverse

---
+++ revers.js
@@ -0,0 +1,14 @@
+var num = 10;
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
+
+// reverse for string
+
+var friends = ["oishi", "maya", "jaynab", "naima", "nikhil", "champa"];
+for (var i = 7; i >= 0; i--) {
+    var friend = friends[i];
+    console.log(friend);
+}
+
