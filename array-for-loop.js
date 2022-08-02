9e3a3b3e933e45243da789bd0181c42a8a067e23
Fatema04 <shadrinmoni15@gmail.com>
2022-08-01 18:10:57 -0700

    array-loop

---
+++ array-for-loop.js
@@ -0,0 +1,23 @@
+// for (var i = 0; i < 20; i++) {
+// console.log(i);}
+
+
+// not to do
+var numbers = [22, 34, 97, 65, 12, 09, 56];
+for (var i = 0; i < 7; i++) {
+    var number = numbers[i]
+    console.log(number);
+
+}
+// Good practice
+var numbers = [22, 34, 97, 65, 12, 09, 56, 44, 87];
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i]
+    console.log(number);
+
+}
+var friends = ["oishi", "maya", "jaynab", "naima", "nikhil", "champa"];
+for (var i = 0; i < friends.length; i++) {
+    var friend = friends[i];
+    console.log(friend);
+}
