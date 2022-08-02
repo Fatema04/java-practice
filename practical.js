8e804aabbce383b431eab7485d1d553c6c630e40
Fatema04 <shadrinmoni15@gmail.com>
2022-08-01 19:11:10 -0700

    PRACTICAL PROBLEM

---
+++ practical.js
@@ -0,0 +1,23 @@
+// problem one
+
+var moneyGiven = 1000;
+var applePrice = 400;
+var grapePrice = 350;
+var expense = applePrice + grapePrice;
+// console.log(expense);
+var moneyBack = moneyGiven - expense;
+console.log(moneyBack);
+
+// problem two
+
+var mathMarks = 4.00;
+var botanyMarks = 3.25;
+var zoologyMarks = 3.25;
+var engMark = 3.75;
+
+var totalMarks = mathMarks + botanyMarks + zoologyMarks + engMark
+
+var avarage = totalMarks / 4;
+var avarageTwoDecimal = avarage.toFixed(2);
+
+console.log(avarageTwoDecimal);
