549539484778b4b17f8c45dc1738047f203d734c
Fatema04 <shadrinmoni15@gmail.com>
2022-08-03 09:16:38 -0700

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+var myDress = {
+    type: "shari",
+    color: "blue",
+    length: "15feet",
+    design: "with white stone",
+    price: 3000
+}
+console.log(myDress);
+console.log(myDress.design);
+
+// if want to change value
+/*myDress.color = 'green';
+ */
