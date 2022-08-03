7e50e850419da03309c99c24eff20a3c128ed429
Fatema04 <shadrinmoni15@gmail.com>
2022-08-03 08:05:33 -0700

    parameter

---
+++ parameter.js
@@ -0,0 +1,27 @@
+function bringCocacola(money) {
+    console.log('give me 1liter cocacola');
+    console.log(money);
+}
+
+bringCocacola(70);
+
+//example 2
+function sum(a, b, c) {
+    console.log(a, b, c);
+    var sum = (a + b + c);
+    console.log(sum);
+
+}
+
+sum(15, 12, 4);
+
+//example 3
+
+function math(a, b, c, d) {
+    console.log(a, b, c, d);
+    var math = (a + b + c) / d;
+    console.log(math);
+
+}
+
+math(15, 12, 3, 10);
