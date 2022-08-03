574a22929366944ee988be59df8d8db195bf474d
Fatema04 <shadrinmoni15@gmail.com>
2022-08-02 18:57:24 -0700

    3 task of challange

---
+++ challange.js
@@ -0,0 +1,26 @@
+//  ajk amar mon valo nei.. 39
+
+// var mood = 1;
+// while (mood < 39) {
+//     console.log("Ajke amar mon valo nei");
+//     mood++;
+// }
+
+// 58 to 98
+// for (var i = 58; i <= 98; i++) {
+//     console.log(i);
+// }
+
+// find odd number 412 to 456
+// var number = 412;
+// while (number <= 456) {
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// find even 581 to 623
+var number = 581;
+while (number <= 623) {
+    console.log(number);
+    number = number + 2;
+}
