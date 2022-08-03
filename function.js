0bde70f0399161440e18af9b3d056968607cf7b7
Fatema04 <shadrinmoni15@gmail.com>
2022-08-03 07:47:15 -0700

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+// function declaration
+
+function startFan() {
+    console.log('stand up');
+    console.log('walk toward the switch board');
+    console.log('press the switch');
+}
+
+//call function
+
+startFan();
