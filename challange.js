d82e846087782b3ae2952a3e5bc46eeb95611a75
Fatema04 <shadrinmoni15@gmail.com>
2022-08-02 19:28:20 -0700

    3 task of challange

--- challange.js
+++ challange.js
@@ -19,8 +19,35 @@
 // }
 
 // find even 581 to 623
-var number = 581;
-while (number <= 623) {
-    console.log(number);
-    number = number + 2;
+// var number = 581;
+// while (number <= 623) {
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// make an array abot what you learn
+// var myLeassons = ['html', 'css', 'bootstrap', 'github', 'tailwind', 'netlify'];
+// console.log(myLeassons);
+
+
+
+// make array mobole then while loop
+
+// var myMobile = 0;
+// while (myMobile < 4) {
+//     console.log('samsung', 'honor-lite', 'mi-phone');
+//     myMobile++;
+// }
+// for loop 30 to 86 & break 44
+
+for (var i = 30; i <= 86; i++) {
+
+
+    console.log(i);
+    if (i === 44) {
+        break;
+    }
 }
+
+
+// array book price more then 200
