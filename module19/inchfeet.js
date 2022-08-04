3862fe32b01e4e38157eb2ec9de627e481529530
Fatema04 <shadrinmoni15@gmail.com>
2022-08-04 09:46:17 -0700

    2nd class done

---
+++ module19/inchfeet.js
@@ -0,0 +1,14 @@
+function inchToFeet(inch) {
+    var getFeet = inch / 12;
+    return getFeet;
+}
+var result = inchToFeet(50);
+console.log(result);
+
+//miles to kilometer
+function mileToKm(miles) {
+    var getKm = miles * 1.609;
+    return getKm;
+}
+var result1 = mileToKm(90);
+console.log(result1);
