de5c7d73537463c09c72083b09c36a0aa0e09e42
Fatema04 <shadrinmoni15@gmail.com>
2022-08-04 07:10:51 -0700

    all  problem solved

--- solution.js
+++ solution.js
@@ -31,3 +31,24 @@ function table(number) {
 }
 table(13);
 table(7);
+
+/**
+ * 5) You are in a hurry to go to your school on time. But when you are crossing the road, the
+traffic signal is red coloured. In this situation, if you try to cross the road, you may be in
+danger.If you notice a yellow coloured traffic signal, you should stop. If you notice a green
+coloured traffic signal, you should cross the road. So write a JS code, where there is a
+variable called signal. Its value can be green, yellow or red & we will get different activities as
+output depending on the variable. So, hurry up.
+ */
+
+var color = 'red';
+
+if (color === 'red') {
+    console.log("Don't cross the road");
+}
+else if (color === 'yellow') {
+    console.log("Please wait for a while");
+}
+else {
+    console.log("Now you can cross the road");
+}
