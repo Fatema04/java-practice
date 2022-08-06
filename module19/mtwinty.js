644eef9dc9515d5854ee5a2441fea57abafb001b
Fatema04 <shadrinmoni15@gmail.com>
2022-08-06 10:39:26 -0700

    2nd class

--- module19/mtwinty.js
+++ module19/mtwinty.js
@@ -1,7 +1,47 @@
 //upper & lower case
 
-const userName = 'ayesha';
-console.log(userName.toUpperCase());
+//const userName = 'ayesha';
+//console.log(userName.toUpperCase());
+//
+//const myName = 'SUPTHI';
+//console.log(myName.toLowerCase()); 
 
-const myName = 'SUPTHI';
-console.log(myName.toLowerCase()); 
+//useof includes type1
+
+//const antheme = "Amar shonar bangla, ami tumay valobashi";
+//const doesExist = antheme.includes("ami");
+//console.log(doesExist);
+
+
+//useof includes type2
+
+//var a = "bangla";
+//var b = antheme.toLowerCase();
+//var c = a.toLowerCase();
+//var doesExist = a.includes(c);
+//console.log(doesExist);
+
+//-----------------------------------------------------------
+//indexof
+
+const antheme = "Amar shonar bangla, ami tumay valobashi";
+console.log(antheme.indexOf("ami"));
+console.log(antheme.length);
+
+
+//-----------------------------------------------------------
+
+
+//startswitch
+console.log(antheme.startsWith("Amar"));
+
+//-------------------------------------------------------------
+
+//endsWith
+
+var fileName = 'bd.png';
+var otherFile = 'bird.jpg';
+console.log(fileName.endsWith(".png"));
+
+
+
