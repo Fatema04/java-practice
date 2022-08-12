a454f86a858faf5298e46b9065fd3264bcaaeccd
Fatema04 <shadrinmoni15@gmail.com>
2022-08-12 09:42:14 -0700

    explore dom

---
+++ module24/one.js
@@ -0,0 +1,30 @@
+//tour of DOM
+
+console.log('welcome');
+console.log(document.body);
+
+//document.getElementsByTagName
+
+/*type1
+    const liCollection = document.getElementsByTagName('li');
+     console.log(liCollection);
+
+type2
+    const liCollection = document.getElementsByTagName('li');
+    for (const li of liCollection) {
+        console.log(li);
+        console.log(li.innerText);
+    }
+//document.getElementsByClassName        
+
+    const classes = document.getElementsByClassName('greet');
+    console.log(classes)
+
+//document.querySelectorAll
+   
+      const allLi = document.querySelectorAll('.container');
+
+    for (const li of allLi) {
+        console.log(li);
+    }*/
+
