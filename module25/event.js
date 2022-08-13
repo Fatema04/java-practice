f010bda89f100e69138cfa89690c459129a54af8
Fatema04 <shadrinmoni15@gmail.com>
2022-08-13 08:28:56 -0700

    addEventListener

--- module25/event.js
+++ module25/event.js
@@ -8,7 +8,7 @@ const blueButton = document.getElementById('blue-button');
 blueButton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
-    blueButton.style.margin = '10px';
+
 }
 
 //purple button
@@ -18,5 +18,25 @@ purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple';
 }
 
+//pink button
 
+const pinkButton = document.getElementById('pink-button');
+pinkButton.addEventListener('click', makePink)
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+//green button
+
+const greenButton = document.getElementById('green-button');
+greenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+//tomato button
+
+document.getElementById('tomato-button').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'tomato';
+})
 
--- module25/summary.js
+++ module25/summary.js
@@ -21,4 +21,32 @@
         }
     </script>
 
+//-----------------------addEventListener---------------------------------//
+
+ <button id="pink-button">Make me pink</button>
+
+ const pinkButton = document.getElementById('pink-button');
+        pinkButton.addEventListener('click', makePink)
+
+        function makePink() {
+            document.body.style.backgroundColor = 'pink';
+        }
+
+//-----------------------addEventListener---------------------------------//
+
+ <button id="green-button">Make me green</button>
+
+ const greenButton = document.getElementById('green-button');
+        greenButton.addEventListener('click', function makeGreen() {
+            document.body.style.backgroundColor = 'green';
+        })
+
+//-----------------------addEventListener---------------------------------//
+
+ <button id="tomato-button">Make me tomato</button>
+
+ document.getElementById('tomato-button').addEventListener('click', function () {
+            document.body.style.backgroundColor = 'tomato';
+        })
+
 */
