b194a1260594650eed4d91281f1b64e25b189846
Fatema04 <shadrinmoni15@gmail.com>
2022-08-27 23:40:00 -0700

    random user

---
+++ milestone-six/module33/random.js
@@ -0,0 +1,23 @@
+function loadUser() {
+    fetch('https://randomuser.me/api/?results=10')
+        .then(res => res.json())
+        .then(data => displayUser(data.results))
+}
+
+
+const displayUser = user => {
+    const userContainer = document.getElementById('user-container');
+    for (const userData of user) {
+        console.log(userData)
+        const div = document.createElement('div');
+        div.classList.add('user')
+        div.innerHTML = `
+        <h3>${userData.id.name}</h3>
+        <h3>${userData.location.city}</h3>
+        <h3>${userData.email}</h3>
+
+        `;
+        userContainer.appendChild(div)
+    }
+}
+loadUser()
