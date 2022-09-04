6fb963820b976b05dfc6e8d9c1347110cd8941b1
Fatema04 <shadrinmoni15@gmail.com>
2022-09-04 10:55:58 -0700

    20% done

--- milestone-six/n/app.js
+++ milestone-six/n/app.js
@@ -1,13 +1,13 @@
 const API_KEY = '7e3471e844ef5ee78fbb016b40214a04'
-const loadWeather = city => {
+const loadTemperature = city => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayTemperature(data))
 }
 
-const displayTem = data => {
+const displayTemperature = data => {
     console.log(data)
     const tempu = document.getElementById('temperature')
     tempu.innerText = data.main.temp;
@@ -16,6 +16,7 @@ document.getElementById('btn-search').addEventListener('click', function () {
 
     const searchField = document.getElementById('input-field');
     const city = searchField.value;
-    loadWeather(city)
+    document.getElementById('city').innerText = city;
+    loadTemperature(city)
 })
-loadWeather()
+loadTemperature('dhaka')
